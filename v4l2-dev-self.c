00f80d4f35ff04ee83703d99ef4e016fd4bf3f12
leesagacious <sensor1010@163.com>
2021-03-07 06:40:24 -0800

    Add register char device.
    Signed-off-by: leesagacious <sensor1010@163.com>

--- v4l2-dev-self.c
+++ v4l2-dev-self.c
@@ -5,3 +5,29 @@
  * No longer use arrays to store.
  */
 static LIST_HEAD(video_self_device)
+
+int register_video_self_device(struct video_self_device *vsd)
+{
+	int ret;
+
+	vsd->cdev = cdev_alloc();
+	if (!vsd->cdev) {
+		ret = -ENOMEM;
+		goto cdev_failed;
+	}
+
+	vsd->cdev->ops = &vsd_fops;
+	vsd->cdev->owner = owner;
+	ret = cdev_add(vsd->cdev, MKDEV(), 1);
+	if (ret < 0) {
+		/*
+		 * not use pr_err.
+		 */
+		pr_debug("cdev_add error %s\n", __func__);
+		kfree(vsd->cdev);
+		vsd->cdev = NULL;
+		goto cdev_failed;
+	}
+
+	return ret;	
+}	
--- v4l2-dev-self.h
+++ v4l2-dev-self.h
@@ -1,5 +1,8 @@
+
 struct video_self_device
 {
 	struct device self_dev;
 	struct cdev *self_cdev;
 };
+
+int register_video_self_device(struct video_self_device *vsd);
